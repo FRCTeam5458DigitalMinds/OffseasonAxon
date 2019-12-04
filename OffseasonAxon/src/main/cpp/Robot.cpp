975971bd0f8e554880f8c407505ff4d9a5179cdf
AyantuT <ayantut2022@gmail.com>
2019-12-03 19:05:07 -0800

    Basic forward/backward

--- OffseasonAxon/src/main/cpp/Robot.cpp
+++ OffseasonAxon/src/main/cpp/Robot.cpp
@@ -5,17 +5,48 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "Robot.h"
-
+#include <string>
+#include <sstream>
+#include <Robot.h>
+#include <frc/WPILib.h>
+#include <stdlib.h>
 #include <iostream>
-
+#include <Encoder.h>
+#include <frc/Timer.h>
+#include <TimedRobot.h>
+#include <frc/Joystick.h>
+#include <ctre/Phoenix.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/drive/DifferentialDrive.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+
+TalonSRX srx = {0};
+
+//Motors
+
+// Right Side Drive Motors
+WPI_TalonSRX RightMotorOne{15}; // Encoder
+WPI_VictorSPX RightMotorTwo{5};
+WPI_VictorSPX RightMotorThree{13};
+// Left Side Drive Motors
+WPI_VictorSPX LeftMotorOne{2};
+WPI_VictorSPX LeftMotorTwo{1};
+WPI_TalonSRX LeftMotorThree{0}; // Encoder
+
+//Joysticks, RaceWheel, and Xbox Controller
+frc::Joystick JoyAccel1{0}, Xbox{1}, RaceWheel{2};
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  srx.Set (ControlMode::PercentOutput, 0);
+
+  RightMotorOne.SetInverted(true);
+  RightMotorTwo.SetInverted(false);
+  RightMotorThree.SetInverted(true);  
 }
+
 
 /**
  * This function is called every robot packet, no matter the mode. Use
@@ -25,7 +56,28 @@ void Robot::RobotInit() {
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic() {
+
+  double JoyY = -JoyAccel1.GetY();
+  double WheelX = RaceWheel.GetX();
+  
+  if (JoyY > 0.1|| JoyY < -0.1 ) {
+    LeftMotorOne.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, JoyY);
+    LeftMotorTwo.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, JoyY);
+    LeftMotorThree.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, JoyY);
+    RightMotorOne.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, JoyY);
+    RightMotorTwo.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, JoyY);
+    RightMotorThree.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, JoyY);                  
+  } else {
+    LeftMotorOne.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+    LeftMotorTwo.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+    LeftMotorThree.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);        
+    RightMotorOne.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+    RightMotorTwo.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+    RightMotorThree.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+  }
+
+}
 
 /**
  * This autonomous (along with the chooser code above) shows how to select
